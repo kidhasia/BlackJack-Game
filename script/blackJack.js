1561fbd812ae954f1a7402cc679760fd41fcb4b1
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-06-15 13:45:57 +0530

    Updated

--- script/blackJack.js
+++ script/blackJack.js
@@ -6,23 +6,32 @@ let isAlive = true;
 let message = "";
 let getMessage = document.getElementById("messageID");
 let getSum = document.getElementById("sumID");
+let getCard = document.getElementById("cardID");
+
 console.log(sum);
 
+function startGame() {
+    getCard.innerText = "Cards: " + firstCard + " - " + secondCard;
+    if (sum < 21) {
+        message = "Do you want another card? ";
+    }
+    else if (sum === 21) {
+        message = "You won!";
+        hasBlackjack = true;
+    }
+    else if (sum > 21) {
+        message = "You lost!";
+        isAlive = false;
+    }
 
-function startGame(){
-if (sum < 21) {
-    message = "Do you want another card? ";
-}
-else if (sum === 21) {
-    message = "You won!";
-    hasBlackjack = true;
-}
-else if (sum > 21) {
-    message = "You lost!";
-    isAlive = false;
-}
+    getMessage.innerText = message;
+    getSum.innerText = "Sum: " + sum;
+   
 
-getMessage.innerText = message;
-getSum.innerText = sum;
+}
+function restartGame(){
+    getSum.innerText = "Sum: ";
+    getCard.innerText = "Cards: " 
+    getMessage.innerText = "Wanna play a round?";
 
 }
